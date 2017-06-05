960a6f859e3ab9b3efaac1d0f4b5d790d7e58c9f
Justin Kimn <djkimchee@gmail.com>
2017-06-05 10:59:09 -0700

    basic react setup

---
+++ src/pages/App/App.jsx
@@ -0,0 +1,28 @@
+import React, {Component} from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+	Redirect
+} from 'react-router-dom';
+import userService from '../../utils/userService';
+import './App.css';
+
+class App extends Component {
+	constructor(props){
+		state(props);
+		this.state = {
+
+		}
+	}
+
+	render(){
+		return (
+			<div>
+
+			</div>
+		)
+	}
+}
+
+export default App;
