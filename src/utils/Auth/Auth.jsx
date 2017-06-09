fa7e38987924930a77f4c82444146902c9c0b931
Justin Kimn <djkimchee@gmail.com>
2017-06-08 20:50:39 -0700

    callback fix:

--- src/utils/Auth/Auth.jsx
+++ src/utils/Auth/Auth.jsx
@@ -7,7 +7,7 @@ export default class Auth {
 		oidcConformant: true,
 		autoclose: true,
 		auth: {
-			redirectUrl: process.env.REACT_APP_AUTH0_CALLBACK_URL,
+			redirectUrl: "war-of-the-memes.herokuapp.com",
 			responseType: 'token id_token',
 			audience: `https://${process.env.REACT_APP_AUTH0_DOMAIN}/userinfo`,
 			params: {
